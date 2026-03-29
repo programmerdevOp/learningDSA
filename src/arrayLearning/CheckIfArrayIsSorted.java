e6106955fbd93298135cd782d2310233638d8d36
sumity29 <enc.sumitpal29@gmail.com>
2026-03-29 11:44:56 +0530

    adding check sorted code

---
+++ src/arrayLearning/CheckIfArrayIsSorted.java
@@ -0,0 +1,22 @@
+package arrayLearning;
+
+public class CheckIfArrayIsSorted {
+    public static void main(String[] args){
+        int[] nums = {2,3,5,7,9};
+        int n = nums.length;
+
+        boolean ans = checkSorted(nums, n);
+
+        System.out.println(ans);
+    }
+
+    static boolean checkSorted(int[] nums, int n){
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] < nums[i+1]){
+                //continue
+            }else return false;
+        }
+
+        return true;
+    }
+}
