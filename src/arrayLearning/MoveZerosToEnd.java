65e1594430f9957b233eb3fc355838b4c571a7b1
sumity29 <enc.sumitpal29@gmail.com>
2026-04-10 00:50:09 +0530

    adding code for move zeros to end

--- src/arrayLearning/MoveZerosToEnd.java
+++ src/arrayLearning/MoveZerosToEnd.java
@@ -2,6 +2,44 @@ package arrayLearning;
 
 public class MoveZerosToEnd {
     public static void main(String[] args) {
-        
+        int[] arr = {1,0,2,4,3,0,1,5,7,0,4,0};
+        moveZero(arr);
+    }
+
+    public static void moveZero(int[] arr){
+        int zeroCount = 0;
+
+        for(int a: arr){
+            if(a == 0) zeroCount++;
+        }
+
+        //System.out.println(zeroCount);
+
+        int[] temp = new int[arr.length];
+
+        int j = 0;
+        for(int i=0;i< arr.length;++i){
+            if(arr[i] != 0){
+                temp[j] = arr[i];
+                j++;
+            }
+        }
+
+        // printing temp arr
+//        for(int num: temp){
+//            System.out.println(num);
+//        }
+
+        // aab j ke andar 0 insert kar de until array.length
+        while(j<temp.length){
+            temp[j] = 0;
+            j++;
+        }
+
+        for(int num: temp){
+            System.out.print(num+" ");
+        }
+
+        return;
     }
 }
