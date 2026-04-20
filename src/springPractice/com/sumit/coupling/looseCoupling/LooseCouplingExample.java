26f7620497b115d526eae7537ad88a1761752d26
sumity29 <enc.sumitpal29@gmail.com>
2026-04-20 23:44:20 +0530

    add

--- src/springPractice/com/sumit/coupling/looseCoupling/LooseCouplingExample.java
+++ src/springPractice/com/sumit/coupling/looseCoupling/LooseCouplingExample.java
@@ -1,4 +1,7 @@
 package springPractice.com.sumit.coupling.looseCoupling;
 
 public class LooseCouplingExample {
+    public static void main(String[] args) {
+        System.out.println("hey");
+    }
 }
