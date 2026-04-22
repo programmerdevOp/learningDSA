4011a4b5dfe93ec2c9aadd91fe244d422dbb7771
sumity29 <enc.sumitpal29@gmail.com>
2026-04-22 23:43:06 +0530

    a

--- src/springPractice/com/sumit/coupling/looseCoupling/LooseCouplingExample.java
+++ src/springPractice/com/sumit/coupling/looseCoupling/LooseCouplingExample.java
@@ -1,7 +1,30 @@
 package springPractice.com.sumit.coupling.looseCoupling;
 
-public class LooseCouplingExample {
-    public static void main(String[] args) {
-        System.out.println("hey");
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+
+        for (String query : queries) {
+
+            for (String s : dictionary) {
+                int diff = 0;
+
+                for (int i = 0; i < query.length(); i++) {
+                    if (query.charAt(i) != s.charAt(i)) {
+                        diff++;
+                    }
+
+                    if (diff > 2)
+                        break;
+                }
+
+                if (diff <= 2) {
+                    result.add(query);
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
