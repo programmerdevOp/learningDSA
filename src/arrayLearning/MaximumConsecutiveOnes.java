a03e92fa857c109d3ea6919513ad60b401848c29
sumity29 <enc.sumitpal29@gmail.com>
2026-05-04 23:40:59 +0530

    add

--- src/arrayLearning/MaximumConsecutiveOnes.java
+++ src/arrayLearning/MaximumConsecutiveOnes.java
@@ -44,4 +44,44 @@ public class MaximumConsecutiveOnes {
 //        }
 //        return false;
 //    }
+class Solution {
+    public void rotate(int[][] matrix) {
+        int N = matrix.length;
+
+        //find transpose
+        for(int i = 0; i < N; i++) {
+            for(int j = i; j < N; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //flip horizontally - For clock wise rotation
+        for(int i = 0; i < N; i++) {
+            int left = 0, right = N - 1;
+            while(left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        /*
+        //flip vertically - For anticlock wise rotation
+        for(int col = 0; col < N; col++) {
+            int row = 0;
+            int mid = N/2;
+            while(row < mid) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[N-row-1][col];
+                matrix[N-row-1][col] = temp;
+                row++;
+            }
+        }
+        */
+    }
+}
 //}
